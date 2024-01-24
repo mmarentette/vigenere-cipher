3bcdcf7cd2b6635ed3eadd02e3eb288114d1fcd3
mmarentette <melrosemarentette@gmail.com>
2024-01-24 13:56:32 +0200

    Find the key for a given keyword

--- vigenere-cipher.rb
+++ vigenere-cipher.rb
@@ -1,8 +1,24 @@
 class VigenereCipher
-    attr_reader :key, :cipher_text
+    attr_reader :keyword, :cipher_text
 
-    def initialize(key, cipher_text)
-        @key = key
-        @cipher_text = cipher_text
+    def initialize(keyword, cipher_text)
+        @keyword = keyword.upcase
+        @cipher_text = cipher_text.upcase
+    end
+
+    def key
+        len = keyword.length
+        result = keyword.split('')
+        i = 0
+
+        cipher_text.split('')[len..-1].each do
+            result.push(keyword[i % len])
+            i += 1
+        end
+
+        result.join('')
     end
 end
+
+cipher = VigenereCipher.new('AYUSH', 'GCYCZFMLYLEIM')
+print cipher.key
